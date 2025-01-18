1233a00708e43db8e2e5db9235cae04add7a7665
Kamal1281 <159178340+Kamal1281@users.noreply.github.com>
2025-01-18 15:44:12 +0000

    compare

---
+++ basic_01/comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 3)  FALSE
+// console.log(2 < 3)   TRUE
+// console.log(2 >= 3)  FALSE
+// console.log(2 <= 3)  TRUE
+// console.log(2 == 3) FALSE
+// console.log(2 === 3) FALSE
+// console.log(2 != 3) TRUE
+
+/*console.log("A" > 5) FALSE
+console.log("A" < 5)    FALSE
+console.log("A" == 5)   FALSE
+console.log("A" >= 5)   FALSE
+console.log("A" <= 5)   FALSE
+console.log("A" != 5)   TRUE  
+*/
+console.log("3" < 4)
+console.log("3" > 4)
+console.log("2" == 2);
+
+
+
