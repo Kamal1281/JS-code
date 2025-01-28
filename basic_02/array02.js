9c037adc9297861ff715cfb9e07646b47795e8fa
Kamal1281 <159178340+Kamal1281@users.noreply.github.com>
2025-01-28 16:12:19 +0000

    array2

---
+++ basic_02/array02.js
@@ -0,0 +1,47 @@
+const fruits = ["apple","mango","banana","papaya"]
+
+// console.log(fruits.length);
+// console.log(fruits[3]);
+// console.log(fruits)
+
+// console.log(fruits.join());
+// console.log(fruits.join("---"));
+// console.log(fruits.join("=>"));
+
+let numb = [33,44,55,66,77,88,99]
+// console.log(numb.slice(2)); 
+// console.log(numb)
+// console.log(numb.slice(2,4));
+
+// console.log(numb.indexOf(77,5)) 
+// console.log(numb.indexOf(77,2))
+
+//push() return length of an array after adding element
+// console.log(numb.push())
+// console.log(numb) 
+// console.log(numb.push(98)) 
+// console.log(numb) 
+// console.log(numb.push(1,2,3,4,5))
+// console.log(numb)
+
+//console.log(Object.keys(fruits)) return indexes of all elements
+
+// console.log(numb.pop())
+// console.log(numb)
+
+let arr3 = fruits.concat(numb)
+// console.log(arr3)
+//  console.log(fruits.concat("orange","guava"))
+// console.log(fruits)
+
+let num1= [1,,3,,5]
+let num2 = [2,,4,,6]
+// let num3 = num1.concat(num2)
+// console.log(num3)
+
+
+
+
+
+
+
